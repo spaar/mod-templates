f7741ae272f2fe1e7d7579274f9c9a2731798c7a
spaar <spaar.modding@gmail.com>
2017-04-17 23:20:37 +0200

    Initial commit

---
+++ BesiegeMod/Mod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using spaar.ModLoader;
+using UnityEngine;
+
+namespace BesiegeMod
+{
+
+  // If you need documentation about any of these values or the mod loader
+  // in general, take a look at https://spaar.github.io/besiege-modloader.
+
+  public class YourMod : Mod
+  {
+    public override string Name { get; } = "<placeholder>";
+    public override string DisplayName { get; } = "<placeholder>";
+    public override string Author { get; } = "<placeholder>";
+    public override Version Version { get; } = new Version(1, 0, 0);
+
+    public override void OnLoad()
+    {
+      // Your initialization code here
+    }
+
+    public override void OnUnload()
+    {
+      // Your code here
+      // e.g. save configuration, destroy your objects if CanBeUnloaded is true etc
+    }
+  }
+}
